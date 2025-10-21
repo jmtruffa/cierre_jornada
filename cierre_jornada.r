d976a3878f75b7e90467402c6662259d0032110d
juan <>
2025-10-21 09:02:45 -0300

    agregado working directory de la VM

--- cierre_jornada.r
+++ cierre_jornada.r
@@ -32,6 +32,7 @@ if (length(args) == 0) {
 } else {
   update <- FALSE
 }
+setwd("~/dev/r/outlier/cierre_jornada")
 functions::setup(server = "GC")
 outlier::theme_outlier()
 path = "/home/jmt/cierre-jornada"
